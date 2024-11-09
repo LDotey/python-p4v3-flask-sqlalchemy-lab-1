910e22ec4e1d1c2fcdafb942e5b3287916712b58
Leigh Dotey <leighdotey@gmail.com>
2024-11-09 13:03:10 -0800

    complete lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,25 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}'
+    
+
+# Add the following attributes to the Earthquake model:
+
+# a string named __tablename__ assigned to the value "earthquakes".
+# a column named id to store an int that is the primary key.
+# a column named magnitude to store a float.
+# a column named location to store a string.
+# A column named year to store an int.
+# Add a __repr__ method to return a string that 
+# formats the attributes id, magnitude, location, 
+# and year as a comma-separated sequence as shown:
